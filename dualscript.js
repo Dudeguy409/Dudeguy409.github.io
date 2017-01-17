7c8e3e5183f742b756e1a168d1e9ce2a047670aa
Andrew Davidson <davidsac@rose-hulman.edu>
2017-01-17 11:59:14 -0800

    added bug repro files

---
+++ dualscript.js
@@ -0,0 +1,8 @@
+function navFrameA(){
+  document.getElementById('framea').src="http://dudeguy409.github.io/";
+}
+function navFrameB(){
+  document.getElementById('frameb').src="http://dudeguy409.github.io/";
+}
+document.getElementById('buttona').addEventListener("click", navFrameA);
+document.getElementById('buttonb').addEventListener("click", navFrameB);
