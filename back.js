1dc877b737cdb78b20c794b442490e0429e9eb75
Andrew Davidson <davidsac@rose-hulman.edu>
2017-02-27 16:36:21 -0800

    Update back.js

--- back.js
+++ back.js
@@ -4,6 +4,9 @@ function goBack() {
 function goBlank() {
   window.location.href="about:blank";
 }
+function goData() {
+  window.location.href="data:text/html,<html><body><h1>big</h1><h5>small</h5></body></html>";
+}
 function navigate() {
   var url = document.getElementById('navinput').value;
   alert(url);
@@ -12,3 +15,4 @@ function navigate() {
 document.getElementById('navbutton').addEventListener("click", navigate);
 document.getElementById('backbutton').addEventListener("click", goBack);
 document.getElementById('blankbutton').addEventListener("click", goBlank);
+document.getElementById('databutton').addEventListener("click", goData);
