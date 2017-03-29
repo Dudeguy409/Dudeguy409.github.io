5662204f35e0debfc8e6801301817c8bb9e1a26b
Andrew Davidson <davidsac@rose-hulman.edu>
2017-03-29 13:17:56 -0700

    Update back.js

--- back.js
+++ back.js
@@ -5,11 +5,11 @@ function goBlank() {
   window.location.href="about:blank";
 }
 function goData() {
-  window.location.href="data:text/html,<html><body><h1>big</h1><h5>small</h5></body></html>";
+  window.location.href="data:text/html,<html><body><h1>data URL big</h1><h5>data URL small</h5></body></html>";
 }
 function navigate() {
   var url = document.getElementById('navinput').value;
-  alert(url);
+  alert("You are being navigated to the following URL that you entered: " + url);
   window.location.href=url;
 }
 document.getElementById('navbutton').addEventListener("click", navigate);
