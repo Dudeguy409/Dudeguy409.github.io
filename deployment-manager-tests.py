febb9529ddffe14a0233573859d1af63961afbdf
Andrew Davidson <davidsac@rose-hulman.edu>
2017-05-12 15:48:57 -0700

    Update deployment-manager-tests.py

--- deployment-manager-tests.py
+++ deployment-manager-tests.py
@@ -31,17 +31,19 @@ Then, before running the tests, set the environment variables for your specific 
 import json
 import os
 import subprocess
-import time
 
 deployment_name = os.environ.get("DEPLOYMENT_MANAGER_TEST_DEPLOYMENT_NAME")
-project_to_create = os.environ.get("DEPLOYMENT_MANAGER_TEST_PROJECT_NAME")
+project_to_create = os.environ.get("DEPLOYMENT_MANAGER_TEST_PROJECT_TO_CREATE")
 organization = os.environ.get("DEPLOYMENT_MANAGER_TEST_ORGANIZATION_ID")
 service_account_a = os.environ.get("DEPLOYMENT_MANAGER_TEST_SERVICE_ACCOUNT_OWNER_A")
 service_account_b = os.environ.get("DEPLOYMENT_MANAGER_TEST_SERVICE_ACCOUNT_OWNER_B")
 service_account_c = os.environ.get("DEPLOYMENT_MANAGER_TEST_SERVICE_ACCOUNT_OWNER_C")
 billing_account = os.environ.get("DEPLOYMENT_MANAGER_TEST_BILLING_ACCOUNT")
 account_to_create = os.environ.get("DEPLOYMENT_MANAGER_TEST_SERVICE_ACCOUNT_TO_CREATE")
+host_project = os.environ.get("DEPLOYMENT_MANAGER_TEST_HOST_PROJECT")
 create_new_project = os.environ.get("DEPLOYMENT_MANAGER_TEST_CREATE_NEW_PROJECT")=="TRUE"
+project_name = project_to_create if create_new_project else host_project
+  
 
 def setup_module(module):
   if create_new_project:
@@ -70,7 +72,10 @@ class TestSimpleDeployment(object):
   need to be interacted with after being deployed in order to ensure that they
   were deployed successfully.
   """
-
+  def create()
+  
+  def delete()
+  
   def deploy(self, deployment_name, yaml_path):
     """Attempts to create and delete a deployment, raising any errors."""
     deployment_create_command = "gcloud deployment-manager deployments create " + deployment_name + " --config examples/v2/" + yaml_path
@@ -101,26 +106,28 @@ class TestSimpleDeployment(object):
     self.deploy("build-config-vm-and-bigquery",
                 "build_configuration/vm_and_bigquery.yaml")
     
-  """  
+  def test_ssl(self):
+    self.deploy("ssl", "ssl/ssl.yaml")
+      
   def test_waiter(self):
     # Replace the placeholder "ZONE_TO_RUN" with an actual zone
     call("sed -i.backup 's/ZONE_TO_RUN/us-west1-b/' examples/v2/waiter/config.yaml")
     self.deploy("waiter", "waiter/config.yaml")
   
-  def test_vpn_auto_subnet(self):
-    # TODO we could probably hack the traditional deploy method to work with this by adding a properties parameter
-    # TODO figure out what values to use for the parameters
-    "gcloud deployment-manager deployments create vpn-auto-subnet --config vpn-auto-subnet.jinja --project PROJECT_NAME --properties \"peerIp=PEER_VPN_IP,sharedSecret=SECRET,sourceRanges=PEERED_RANGE\""
-
-  
-  def test_ssl(self):
-    self.deploy("ssl", "ssl/ssl.yaml")
-    
   def test_vm_startup_script(self):
     # TODO may want to refactor my deploy method to be broken up into two separate methods to test that the script is actually working
     # TODO create an SSH tunnel to connect to "gcloud compute instances describe the-first-vm | grep "natIP""
     # TODO test both versions
     pass
+  
+  def test_quick_start(self):
+    self.deploy("quick_start", "quick_start/vm.yaml")
+  
+  """
+  def test_vpn_auto_subnet(self):
+    # TODO we could probably hack the traditional deploy method to work with this by adding a properties parameter
+    # TODO figure out what values to use for the parameters
+    "gcloud deployment-manager deployments create vpn-auto-subnet --config vpn-auto-subnet.jinja --project PROJECT_NAME --properties \"peerIp=PEER_VPN_IP,sharedSecret=SECRET,sourceRanges=PEERED_RANGE\""
   
   def test_step_by_step_2(self):
     call("sed -i.backup 's/\[MY_PROJECT\]/" + project_to_create + "/' examples/v2/step_by_step_guide/step2_create_a_configuration/two-vms.yaml")
@@ -137,38 +144,34 @@ class TestSimpleDeployment(object):
     self.deploy("step_by_step_5_jinja", "step_by_step_guide/step5_create_a_template/jinja/two-vms.yaml")
     
   def test_step_by_step_6(self):
-    call("sed -i.backup 's/\[MY_PROJECT\]/" + project_to_create + "/' examples/v2/step_by_step_guide/step6_use_multiple_templates/jinja/two-vms.yaml")
-    call("sed -i.backup 's/\[MY_PROJECT\]/" + project_to_create + "/' examples/v2/step_by_step_guide/step6_use_multiple_templates/python/two-vms.yaml")
-    self.deploy("step_by_step_6_python", "step_by_step_guide/step6_use_multiple_templates/python/two-vms.yaml")
-    self.deploy("step_by_step_6_jinja", "step_by_step_guide/step6_use_multiple_templates/jinja/two-vms.yaml")
+    call("sed -i.backup 's/\[MY_PROJECT\]/" + project_to_create + "/' examples/v2/step_by_step_guide/step6_use_multiple_templates/jinja/config-with-many-templates.yaml")
+    call("sed -i.backup 's/\[MY_PROJECT\]/" + project_to_create + "/' examples/v2/step_by_step_guide/step6_use_multiple_templates/python/config-with-many-templates.yaml")
+    self.deploy("step_by_step_6_python", "step_by_step_guide/step6_use_multiple_templates/python/config-with-many-templates.yaml")
+    self.deploy("step_by_step_6_jinja", "step_by_step_guide/step6_use_multiple_templates/jinja/config-with-many-templates.yaml")
     
   def test_step_by_step_7(self):
-    self.deploy("step_by_step_7_python", "step_by_step_guide/step7_use_environment_variables/python/two-vms.yaml")
-    self.deploy("step_by_step_7_jinja", "step_by_step_guide/step7_use_environment_variables/jinja/two-vms.yaml")
+    self.deploy("step_by_step_7_python", "step_by_step_guide/step7_use_environment_variables/python/config-with-many-templates.yaml")
+    self.deploy("step_by_step_7_jinja", "step_by_step_guide/step7_use_environment_variables/jinja/config-with-many-templates.yaml")
     
   def test_step_by_step_8_9(self):
     # TODO may want to refactor my deploy method to be broken up into two separate methods to test that the script is actually working
    
-    self.create("step_by_step_8_9_python", "step_by_step_guide/step8_metadata_and_startup_scripts/python/two-vms.yaml")
-    self.create("step_by_step_8_9_jinja", "step_by_step_guide/step8_metadata_and_startup_scripts/jinja/two-vms.yaml")
+    self.create("step_by_step_8_9_python", "step_by_step_guide/step8_metadata_and_startup_scripts/python/config-with-many-templates.yaml")
+    self.create("step_by_step_8_9_jinja", "step_by_step_guide/step8_metadata_and_startup_scripts/jinja/config-with-many-templates.yaml")
     
      # TODO create an SSH tunnel to connect to "gcloud compute instances describe the-first-vm | grep "natIP""
      
-    self.update("step_by_step_8_9_python", "step_by_step_guide/step9_update_a_deployment/python/two-vms.yaml")
-    self.update("step_by_step_8_9_jinja", "step_by_step_guide/step9_update_a_deployment/jinja/two-vms.yaml")
+    self.update("step_by_step_8_9_python", "step_by_step_guide/step9_update_a_deployment/python/config-with-many-templates.yaml")
+    self.update("step_by_step_8_9_jinja", "step_by_step_guide/step9_update_a_deployment/jinja/config-with-many-templates.yaml")
     
     #TODO reset the vms: gcloud compute instances reset the-first-vm
     # TODO create an SSH tunnel to connect to "gcloud compute instances describe the-first-vm | grep "natIP"", and check the contents
     #TODO delete
     
   def test_step_by_step_10(self):
-    self.create("step_by_step_10_python", "step_by_step_guide/step10_use_python_templates/python/two-vms.yaml")
-    self.create("step_by_step_10_jinja", "step_by_step_guide/step10_use_python_templates/jinja/two-vms.yaml")
+    self.create("step_by_step_10_python", "step_by_step_guide/step10_use_python_templates/python/use-python-template-with-modules.yaml")
+    self.create("step_by_step_10_jinja", "step_by_step_guide/step10_use_python_templates/jinja/use-jinja-template-with-modules.yaml")
     # TODO create an SSH tunnel to connect to "gcloud compute instances describe the-first-vm | grep "natIP""
-    
-    
-  def test_quick_start(self):
-    self.deploy("quick_start", "quick_start/vm.yaml")
   """
   
   
