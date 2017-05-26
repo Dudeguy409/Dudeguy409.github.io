08277806881ed53adc3d7cff1db330b18d7e2e5f
Andrew Davidson <davidsac@rose-hulman.edu>
2017-05-26 11:06:59 -0700

    Update deployment-manager-tests.py

--- deployment-manager-tests.py
+++ deployment-manager-tests.py
@@ -38,6 +38,7 @@ import json
 import os
 import subprocess
 import yaml
+import time
 from nose.tools import timed
 
 # The variables immediately below are used to create a new project in which to make test deployments, but only if the environment variable "DM_TEST_CREATE_NEW_PROJECT" is set to "TRUE".  Please see the example instructions on GitHub to see what value to assign each variable:
