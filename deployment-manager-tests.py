50dc667488778c0cb4daff29d15a51c04a6c0989
Andrew Davidson <davidsac@rose-hulman.edu>
2017-05-30 10:50:29 -0700

    Update deployment-manager-tests.py

--- deployment-manager-tests.py
+++ deployment-manager-tests.py
@@ -422,9 +422,9 @@ class TestSimpleDeployment(object):
     # TODO all the steps specifically mentioned in the readme are performed, but perhaps there are still more things to be done to check that it works?
     call("gcloud compute instance-groups unmanaged set-named-ports " + deployment_name + "-frontend-pri-igm --named-ports http:8080,httpstatic:8080 --zone " + default_zone)
     call("gcloud compute instance-groups unmanaged set-named-ports " + deployment_name + "-frontend-sec-igm --named-ports http:8080,httpstatic:8080 --zone " + secondary_zone)
-    print call("gcloud compute forwarding-rules list | grep application-" + deployment_name + "-l7lb")
+    raise Exception( call("gcloud compute forwarding-rules list | grep application-" + deployment_name + "-l7lb"))
     delete_deployment(deployment_name)
- 
+
   def test_nodejs_l7_python(self):
     secondary_zone = "us-central1-f"
     replace_placeholder_in_file("SECOND_ZONE_TO_RUN", secondary_zone, "nodejs_l7/python/application.yaml")
@@ -435,7 +435,7 @@ class TestSimpleDeployment(object):
     # TODO all the steps specifically mentioned in the readme are performed, but perhaps there are still more things to be done to check that it works?
     call("gcloud compute instance-groups unmanaged set-named-ports " + deployment_name + "-frontend-pri-igm --named-ports http:8080,httpstatic:8080 --zone " + default_zone)
     call("gcloud compute instance-groups unmanaged set-named-ports " + deployment_name + "-frontend-sec-igm --named-ports http:8080,httpstatic:8080 --zone " + secondary_zone)
-    print call("gcloud compute forwarding-rules list | grep application-" + deployment_name + "-l7lb")
+    raise Exception( call("gcloud compute forwarding-rules list | grep application-" + deployment_name + "-l7lb"))
     delete_deployment(deployment_name)
     
   def test_vm_with_disks_jinja(self):
