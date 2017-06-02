017af233d818b77d1ea201fe145fbda30832b4b3
Andrew Davidson <davidsac@rose-hulman.edu>
2017-06-02 15:03:02 -0700

    Update deployment-manager-tests.py

--- deployment-manager-tests.py
+++ deployment-manager-tests.py
@@ -220,7 +220,7 @@ class TestSimpleDeployment(unittest.TestCase):
     for replacement in parameters.get("replace-placeholders"):
       replace_with = replacement["replace-with"]
       if environment.get(replace_with):
-        replace_with = environment.get(eplace_with)
+        replace_with = environment.get(replace_with)
       replace_placeholder_in_file(replacement["search-for"], replace_with, replacement["file-to-modify"])
     if parameters.get("http-server"):
       deploy_http_server(deployment_name, parameters["config-path"])
