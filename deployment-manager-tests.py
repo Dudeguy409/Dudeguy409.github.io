c45ee19ed2d4e2ea2a895394650428b1ae2e1e09
Andrew Davidson <davidsac@rose-hulman.edu>
2017-06-05 16:36:47 -0700

    Update deployment-manager-tests.py

--- deployment-manager-tests.py
+++ deployment-manager-tests.py
@@ -449,7 +449,7 @@ if __name__ == "__main__":
   host_project = args.host_project[0]
   create_new_project = args.create_new_project
   project_name = new_proj_name if create_new_project else host_project
-  default_zone = args.default_zone[0]
+  default_zone = args.default_zone
   ssh_tunnel_port = args.ssh_tunnel_port
   
   print new_proj_deployment_name, new_proj_name, new_proj_org, new_proj_service_account_a, new_proj_service_account_b, new_proj_billing_account, new_proj_account_to_create, host_project, create_new_project, project_name, default_zone, ssh_tunnel_port, environment
